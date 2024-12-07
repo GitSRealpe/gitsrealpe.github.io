ae591161f29b5b4605b98de4b4cd77555b3c75b8
GitSRealpe <msn-realpe@hotmail.com>
2024-12-07 21:13:59 +0100

    click for ooiiao

--- js/main.js
+++ js/main.js
@@ -1,13 +1,47 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+function onDocumentMouseDown(event) {
+    sound.play();
+    if (action) {
+        action.reset(); // Reset to the beginning
+        action.time = 1.0;   // Start from 1 second (adjust as needed)
+        action.play();  // Play the animation
+    }
+}
+
+function onDocumentMouseUp(event) {
+    sound.stop();
+    if (action) {
+        action.stop();  // Stop the animation
+    }
+}
+
+
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
+renderer.domElement.addEventListener('mousedown', onDocumentMouseDown, false);
+renderer.domElement.addEventListener('mouseup', onDocumentMouseUp, false);
+
+// Add AudioListener to the camera
+const listener = new THREE.AudioListener();
+camera.add(listener);
+// Create a global audio source
+const sound = new THREE.Audio(listener);
+// Load an audio file and set it as the Audio object's buffer
+const audioLoader = new THREE.AudioLoader();
+
+audioLoader.load('assets/ooiiao1.ogg', function (buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(false);
+    sound.setVolume(1.0);
+
+});
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
@@ -17,17 +51,15 @@ camera.position.z = 1;
 
 const loader = new GLTFLoader();
 let mixer;
+let action;
 loader.load('assets/scene.gltf', function (gltf) {
-
     const model = gltf.scene;
     scene.add(model);
-
     mixer = new THREE.AnimationMixer(model);
-
     gltf.animations.forEach((clip) => {
-        mixer.clipAction(clip).play();
+        action = mixer.clipAction(clip);
     });
-
+    renderer.setAnimationLoop(animate);
 }, undefined, function (error) {
     console.error(error);
 });
@@ -42,5 +74,8 @@ function animate() {
 
     if (mixer) mixer.update(0.01);
 
+    if (!sound.isPlaying) {
+        action.stop();
+    }
     renderer.render(scene, camera);
 }
